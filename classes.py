a15ff9d1b00febe30d942aac1a47a90e25c5e430
donbassik1228 <148227783+donbassik1228@users.noreply.github.com>
2024-02-07 14:07:40 +0200

    second commit? fixed in str

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 # from datetime import datetime
 # from collections import UserDict
+# from itertools import islice
 
-# class Field:
+# class Field(UserDict):
 #     def __init__(self, value):
 #         if not self.is_valid(value):
 #             raise ValueError("Invalid value")
@@ -66,12 +67,6 @@
 #     def remove_phone(self, phone_number):
 #         self.phones = [phone for phone in self.phones if phone.value != phone_number]
 
-#     def find_phone(self, phone_number):
-#         for phone in self.phones:
-#             if phone.value == phone_number:
-#                 return phone
-#         return None
-
 #     def edit_phone(self, old_phone_number, new_phone_number):
 #         if not Phone.is_valid(new_phone_number):
 #             raise ValueError("Invalid phone number")
@@ -99,19 +94,15 @@
 
 # class AddressBook(UserDict):
 #     def __iter__(self, page_size=5):
-#         current_page = 0
 #         total_records = len(self.data)
-#         while current_page * page_size < total_records:
-#             yield list(self.data.values())[current_page * page_size: (current_page + 1) * page_size]
-#             current_page += 1
+#         for i in range(0, total_records, page_size):
+#             yield list(islice(self.data.values(), i, i + page_size))
 
 #     def add_record(self, record):
 #         if record.name.value in self.data:
-#             return self.data[record.name.value] 
+#             raise ValueError("Record with the same name already exists")
 #         self.data[record.name.value] = record
-#         return record 
-    
-
+#         return record
 
 #     def find(self, name):
 #         return self.data.get(name)
@@ -119,39 +110,13 @@
 #     def delete(self, name):
 #         if name in self.data:
 #             del self.data[name]
-#     def __str__(self) -> str:
+
+#     def __str__(self):
 #         return "\n".join(str(r) for r in self.data.values())
-# book = AddressBook()
 
-# john_record = Record("John", phone_numbers=["1234567890", "5555555555"], birthday="1990-01-15")
-# book.add_record(john_record)
 
-# jane_record = Record("Jane", phone_numbers=["9876543210"], birthday="1985-05-20")
-# book.add_record(jane_record)
 
-# for page_number, page_records in enumerate(book):
-#     print(f"Page {page_number + 1}:")
-#     for record in page_records:
-#         print(record)
-#     print("\n")
 
-# john = book.find("John")
-# john.edit_phone("1234567890", "1112223333")
-
-# print(john)
-
-# try:
-#     found_phone = john.find_phone("5555555555")
-#     print(f"{john.name.value}: {found_phone}")
-# except ValueError as e:
-#     print(e)
-
-# try:
-#     john.edit_phone("1111111111", "9999999999")
-# except ValueError as e:
-#     print(e)
-
-# book.delete("Jane")
 
 from datetime import datetime
 from collections import UserDict
@@ -164,7 +129,7 @@ class Field(UserDict):
         self.__value = value
 
     def __str__(self):
-        return str(self.__value)
+        return f"{self.__class__.__name__}: {self.__value}"
 
     def is_valid(self, value):
         return True
@@ -187,6 +152,9 @@ class Phone(Field):
     def is_valid(phone_number):
         return len(phone_number) == 10 and phone_number.isdigit()
 
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.value}"
+
 class Birthday(Field):
     @staticmethod
     def is_valid(value):
@@ -206,6 +174,9 @@ class Birthday(Field):
 
         days_left = (next_birthday - today).days
         return days_left
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.value}, Days to Birthday: {self.days_to_birthday()}"
 
 class Record:
     def __init__(self, name, phone_number=None, birthday=None):
@@ -244,8 +215,9 @@ class Record:
         return None
 
     def __str__(self):
-        phones_str = '; '.join(str(phone) for phone in self.phones)
-        return f"Contact name: {self.name.value}, phones: {phones_str}, birthday: {self.birthday}"
+        phones_str = ', '.join(str(phone) for phone in self.phones)
+        birthday_str = f", Birthday: {self.birthday}" if self.birthday else ""
+        return f"Record: {self.name}, Phones: {phones_str}{birthday_str}"
 
 class AddressBook(UserDict):
     def __iter__(self, page_size=5):
@@ -268,7 +240,6 @@ class AddressBook(UserDict):
 
     def __str__(self):
         return "\n".join(str(r) for r in self.data.values())
-
 
 
 
